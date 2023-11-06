f929990fc9fd22dcdcf825dffb1976f44c274205
leeseunghan713 <127086663+leeseunghan713@users.noreply.github.com>
2023-11-06 16:08:22 +0900

    🔒 Feat) HTTP_security

--- back/app.js
+++ back/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
+
+app.use(cors());                    //express에서 HTTP 보안을 위해 cors 사용
+app.use(express.json());            //express에서 json을 사용하도록 설정
 
 app.get("/", (req, res) => {
   res.set({ "Content-Type": "text/html; charset=utf-8" });
