43ecc5a123c2b2501d6c1c57a0c14b7c40c7bff8
leeseunghan713 <127086663+leeseunghan713@users.noreply.github.com>
2023-11-07 19:32:06 +0900

    back) server1
    cors(HTTP보안) 과 mysql2(db연동) 설치 및 적용

--- back/app.js
+++ back/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: '1234',
+  database: 'TESTDB'
+});
+
+connection.connect((error) => {
+  if (error) {
+   console.error('연결 실패:', error);
+   return;
+   }
+   console.log('연결 성공!')
+});
+
+app.use(cors());                    //express에서 HTTP 보안을 위해 cors 사용
+app.use(express.json());            //express에서 json을 사용하도록 설정
 
 app.get("/", (req, res) => {
   res.set({ "Content-Type": "text/html; charset=utf-8" });
