9c31989d8255dc4cefb48a5d0b7ff3354c1aef94
leeseunghan713 <127086663+leeseunghan713@users.noreply.github.com>
2023-11-16 11:03:25 +0900

    back) user_funtion
    내 정보 페이지 기능 추가,
    좋아요 버튼 기능 추가

--- back/app.js
+++ back/app.js
@@ -1,49 +1,71 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
-const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
 
+const app = express();
 const createConnection = require('./dbConnection');
 const connection = createConnection();
+const port = 3000;
 
 app.use(cors());                    //express에서 HTTP 보안을 위해 cors 사용
 app.use(express.json());            //express에서 json을 사용하도록 설정
+app.use(bodyParser.json()); 
 
 
+//--------------------------------좋아요 기능----------------------------------------
+// festival_info의 like 속성 값 증가 감소, LIKE 테이블의 festival_id,user_id 갑 삽입
+app.put('/festivals/:festival_id/like/:user_id', (req, res) => {
+  const festival_id = req.params.festival_id;
+  const user_id = req.params.user_id;
+  const action = req.body.action; // 좋아요 추가인지 취소인지를 나타내는 값
 
-// 해당 축제에 좋아요를 누르면 좋아요 값이 1씩 증가
-app.post('/like/:festival_id', (req, res) => {
-  const { festival_id } = req.body;  
+  if (action === 'like') {
+    connection.query('UPDATE festival_info SET `like` = `like` + 1 WHERE festival_id = ?', [festival_id], (error, results, fields) => {
+      if (error) {
+        console.error('카운트 증가 실패:', error);
+        res.status(500).json({ error: '카운트 증가 실패' });
+        return;
+      }
+      console.log('카운트 증가 완료');
 
-  connection.query('UPDATE festival_info SET `like` = `like` + 1', (error, results, fields) => {
-    if (error) {
-      console.error('카운트 증가 실패:', error);
-      res.status(500).json({ error: '카운트 증가 실패' });
-      return;
-    }
-    console.log('카운트 증가 완료');
-    res.status(200).json({ message: '카운트 증가 완료' });
-  });
+      connection.query('INSERT INTO `like1` VALUES (?, ?)', [user_id, festival_id], (error, results, fields) => {
+        if (error) {
+          console.error('LIKE1 테이블에 삽입 실패', error);
+          res.status(500).json({ error: 'LIKE1 테이블에 삽입 실패' });
+          return;
+        }
+        console.log('LIKE1 테이블에 삽입 성공');
+        res.status(200).json({ message: 'LIKE1 테이블에 삽입 성공' });
+      });
+    });
+  } else if (action === 'unlike') {
+    connection.query('UPDATE festival_info SET `like` = `like` - 1 WHERE festival_id = ?', [festival_id], (error, results, fields) => {
+      if (error) {
+        console.error('카운트 감소 실패:', error);
+        res.status(500).json({ error: '카운트 감소 실패' });
+        return;
+      }
+      console.log('카운트 감소 완료');
+
+      connection.query('DELETE FROM `like1` WHERE user_id = ? AND festival_id = ?', [user_id, festival_id], (error, results, fields) => {
+        if (error) {
+          console.error('like1 테이블에서 행 삭제 실패:', error);
+          res.status(500).json({ error: 'like1 테이블에서 행 삭제 실패' });
+          return;
+        }
+        console.log('LIKE1 테이블에서 행 삭제 완료');
+        res.status(200).json({ message: 'like1 테이블에서 행 삭제 완료' });
+      });
+    });
+  } else {
+    res.status(400).json({ error: '잘못된 요청입니다.' });
+  }
 });
 
-// 해당 축제에 좋아요를 취소하면 좋아요 값이 1씩 감소
-app.post('/unlike/:festival_id', (req, res) => {   
-  const { festival_id } = req.body;  
 
-  connection.query('UPDATE festival_info SET `like` = `like` - 1', (error, results, fields) => {
-    if (error) {
-      console.error('카운트 감소 실패:', error);
-      res.status(500).json({ error: '카운트 감소 실패' });
-      return;
-    }
-    console.log('카운트 감소 완료');
-    res.status(200).json({ message: '카운트 감소 완료' });
-  });
-});
-
-// 축제 좋아요 조회
-app.get('/likeCount/:festival_id', (req, res) => {   // http://localhost:3000/likeCount/1234 와 같이 GET방식으로 보내야함
+// 축제 좋아요 갯수 조회
+app.get('/festivals/:festival_id/like-count', (req, res) => {   // http://localhost:3000/likeCount/1234 와 같이 GET방식으로 보내야함
   const festivalId = req.params.festival_id; // 클라이언트에서 festiverId 값을 URL 경로로 전달받음
 
   connection.query('SELECT `LIKE` FROM festival_info WHERE FESTIVAL_ID = ?', [festivalId], (error, results, fields) => {
@@ -57,7 +79,92 @@ app.get('/likeCount/:festival_id', (req, res) => {   // http://localhost:3000/li
   });
 });
 
+//-------------------------------내 정보 페이지----------------------------------------
+//프로필 사진 조회
+app.get('/users/:user_id/image', (req, res) => {
+  const userId = req.params.user_id;
 
+  connection.query('SELECT `image` FROM user_info WHERE user_id = ?', [userId], (error, results, fields) => {
+    if (error) {
+      console.error('프로필 사진 조회 실패:', error);
+      res.status(500).json({ error: '프로필 사진 조회 실패' });
+      return;
+    }
+
+    if (results.length === 0) {
+      console.log('프로필 사진이 없습니다');
+      res.status(404).json({ message: '프로필 사진이 없습니다' });
+      return;
+    }
+
+    const image = results[0].image;
+    console.log('프로필 사진 조회 성공');
+    res.status(200).json({ image });
+  });
+});
+
+// 회원 정보 수정 (닉네임 변경)
+app.put('/users/:user_id/nickname', (req, res) => {
+  const userId = req.params.user_id;
+  const newNickname = req.body.nickname;
+
+  connection.query('UPDATE user_info SET nickname = ? WHERE user_id = ?', [newNickname, userId], (error, results, fields) => {
+    if (error) {
+      console.error('닉네임 변경 실패:', error);
+      res.status(500).json({ error: '닉네임 변경 실패' });
+      return;
+    }
+    console.log('닉네임 변경 성공');
+    res.status(200).json({ message: '닉네임 변경 성공' });
+  });
+});
+
+// 회원 정보 수정 (프로필 사진 변경)
+app.put('/users/:user_id/image', (req, res) => {
+  const userId = req.params.user_id;
+  const newImage = req.body.image;
+
+  connection.query('UPDATE user_info SET image = ? WHERE user_id = ?', [newImage, userId], (error, results, fields) => {
+    if (error) {
+      console.error('프로필 사진 변경 실패:', error);
+      res.status(500).json({ error: '프로필 사진 변경 실패' });
+      return;
+    }
+    console.log('프로필 사진 변경 성공');
+    res.status(200).json({ message: '프로필 사진 변경 성공' });
+  });
+});
+
+// 회원 정보 수정(알림 설정)
+app.put('/users/:user_id/notice', (req, res) => {
+  const user_id = req.params.user_id;
+  const notice = req.body.notice;
+
+  connection.query('UPDATE user_info SET notice = ? WHERE user_id = ?', [notice, user_id], (error, results, fields) => {
+    if (error) {
+      console.error('알림 설정 변경 실패:', error);
+      res.status(500).json({ error: '알림 설정 변경 실패' });
+      return;
+    }
+    console.log('알림 설정 변경');
+    res.status(200).json({ message: '알림 설정 변경' });
+  });
+});
+
+// 회원 탈퇴 기능
+app.delete('/users/:user_id', (req, res) => {
+  const userId = req.params.user_id;
+
+  connection.query('DELETE FROM user_info WHERE user_id = ?', [userId], (error, results, fields) => {
+    if (error) {
+      console.error('회원 탈퇴 실패:', error);
+      res.status(500).json({ error: '회원 탈퇴 실패' });
+      return;
+    }
+  
+    res.status(200).json({ message: '회원 탈퇴가 성공적으로 처리되었습니다.' });
+  });
+});
 
 
 
